2ce2b23e5afba88b759b955bc5bf487c12a6219b
Bertrand Songis <bsongis@gmail.com>
2017-12-27 23:54:59 +0100

    X-Lite board support continued

--- radio/src/gui/128x64/model_setup.cpp
+++ radio/src/gui/128x64/model_setup.cpp
@@ -53,12 +53,12 @@ enum MenuModelSetupItems {
   CASE_CPUARM(ITEM_MODEL_CHECKLIST_DISPLAY)
   ITEM_MODEL_THROTTLE_WARNING,
   ITEM_MODEL_SWITCHES_WARNING,
-#if defined(PCBX7)
+#if defined(PCBTARANIS)
   ITEM_MODEL_POTS_WARNING,
 #endif
   ITEM_MODEL_BEEP_CENTER,
   CASE_CPUARM(ITEM_MODEL_USE_GLOBAL_FUNCTIONS)
-#if defined(PCBX7)
+#if defined(PCBTARANIS)
   ITEM_MODEL_INTERNAL_MODULE_LABEL,
   ITEM_MODEL_INTERNAL_MODULE_MODE,
   ITEM_MODEL_INTERNAL_MODULE_CHANNELS,
@@ -116,7 +116,9 @@ enum MenuModelSetupItems {
 #define MODEL_SETUP_RANGE_OFS            4*FW+3
 #define MODEL_SETUP_SET_FAILSAFE_OFS     7*FW-2
 
-#if defined(PCBX7)
+#if defined(PCBXLITE)
+  #define CURRENT_MODULE_EDITED(k)       (k>=ITEM_MODEL_EXTERNAL_MODULE_LABEL ? EXTERNAL_MODULE : INTERNAL_MODULE)
+#elif defined(PCBX7)
   #define CURRENT_MODULE_EDITED(k)       (k>=ITEM_MODEL_TRAINER_LABEL ? TRAINER_MODULE : (k>=ITEM_MODEL_EXTERNAL_MODULE_LABEL ? EXTERNAL_MODULE : INTERNAL_MODULE))
 #elif defined(PCBSKY9X) && !defined(REVA)
   #define CURRENT_MODULE_EDITED(k)       (k>=ITEM_MODEL_EXTRA_MODULE_LABEL ? EXTRA_MODULE : EXTERNAL_MODULE)
@@ -230,7 +232,7 @@ void onBindMenu(const char * result)
 
 void menuModelSetup(event_t event)
 {
-#if defined(PCBX7)
+#if defined(PCBTARANIS)
   MENU_TAB({ HEADER_LINE_COLUMNS 0, TIMER_ROWS, TIMER_ROWS, TIMER_ROWS, 0, 1, 0, 0, 0, 0, 0, CASE_CPUARM(LABEL(PreflightCheck)) CASE_CPUARM(0) 0, NUM_SWITCHES-1,  NUM_POTS, NUM_STICKS+NUM_POTS+NUM_SLIDERS+NUM_ROTARY_ENCODERS-1, 0,
   LABEL(InternalModule),
   INTERNAL_MODULE_MODE_ROWS,
@@ -534,7 +536,7 @@ void menuModelSetup(event_t event)
                 g_model.switchWarningEnable ^= (1 << menuHorizontalPosition);
                 storageDirty(EE_MODEL);
 #else
-#if defined(PCBX7)
+#if defined(PCBTARANIS)
                 if (menuHorizontalPosition < NUM_SWITCHES) {
                   g_model.switchWarningEnable ^= (1 << (menuHorizontalPosition-1));
 #else
@@ -552,7 +554,7 @@ void menuModelSetup(event_t event)
                 g_model.switchWarningState = switches_states;
                 AUDIO_WARNING1();
                 storageDirty(EE_MODEL);
-#elif defined(PCBX7)
+#elif defined(PCBTARANIS)
                 if (attr && menuHorizontalPosition == 0) {
                   getMovedSwitch();
                   g_model.switchWarningState = switches_states;
@@ -574,7 +576,7 @@ void menuModelSetup(event_t event)
           }
         }
         LcdFlags line = attr;
-#if defined(PCBX7)
+#if defined(PCBTARANIS)
         int current = 0;
         for (int i=0; i<NUM_SWITCHES-1; i++) {
           if (SWITCH_WARNING_ALLOWED(i)) {
@@ -590,7 +592,7 @@ void menuModelSetup(event_t event)
         if (attr && menuHorizontalPosition == 0) {
           lcdDrawFilledRect(MODEL_SETUP_2ND_COLUMN-1, y-1, (NUM_SWITCHES-1)*(2*FW), 1+FH*((current+7)/8));
         }
-#else // PCBX7
+#else
         for (uint8_t i=0; i<NUM_SWITCHES-1/*not on TRN switch*/; i++) {
           uint8_t swactive = !(g_model.switchWarningEnable & 1 << i);
           attr = 0;
@@ -617,10 +619,10 @@ void menuModelSetup(event_t event)
           lcdDrawText(MODEL_SETUP_2ND_COLUMN+(NUM_SWITCHES*FW), y, PSTR("<]"), (menuHorizontalPosition == NUM_SWITCHES-1 && !NO_HIGHLIGHT()) ? line : 0);
 #endif
         }
-#endif // PCBX7
+#endif
         break;
       }
-#if defined(PCBX7)
+#if defined(PCBTARANIS)
       case ITEM_MODEL_POTS_WARNING:
         lcdDrawTextAlignedLeft(y, STR_POTWARNING);
         lcdDrawTextAtIndex(MODEL_SETUP_2ND_COLUMN, y, PSTR("\004""OFF\0""Man\0""Auto"), g_model.potsWarnMode, (menuHorizontalPosition == 0) ? attr : 0);
@@ -667,7 +669,7 @@ void menuModelSetup(event_t event)
           }
         }
         break;
-#endif // PCBX7
+#endif
 
       case ITEM_MODEL_BEEP_CENTER:
         lcdDrawTextAlignedLeft(y, STR_BEEPCTR);
@@ -695,7 +697,7 @@ void menuModelSetup(event_t event)
         break;
 #endif
 
-#if defined(PCBX7)
+#if defined(PCBTARANIS)
       case ITEM_MODEL_INTERNAL_MODULE_LABEL:
         lcdDrawTextAlignedLeft(y, TR_INTERNALRF);
         break;
@@ -898,6 +900,8 @@ void menuModelSetup(event_t event)
 
 #if defined(PCBX7)
       case ITEM_MODEL_TRAINER_CHANNELS:
+#endif
+#if defined(PCBTARANIS)
       case ITEM_MODEL_INTERNAL_MODULE_CHANNELS:
 #endif
 #if defined(PCBSKY9X)
@@ -934,6 +938,8 @@ void menuModelSetup(event_t event)
 
 #if defined(PCBX7)
       case ITEM_MODEL_TRAINER_PARAMS:
+#endif
+#if defined(PCBTARANIS)
       case ITEM_MODEL_INTERNAL_MODULE_BIND:
 #endif
 #if defined(PCBSKY9X)
@@ -1093,7 +1099,7 @@ void menuModelSetup(event_t event)
       }
 #endif
 
-#if defined(PCBX7)
+#if defined(PCBTARANIS)
       case ITEM_MODEL_INTERNAL_MODULE_FAILSAFE:
 #endif
 #if defined(CPUARM)
